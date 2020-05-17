cbf453b7692d51e43514d87ffeff4f5c06573562
phicks22 <65268541+phicks22@users.noreply.github.com>
2020-05-16 19:11:21 -0700

    Added Animal Containers and Main packages

--- Laboratory_Feeding/Model/Animal.py
+++ Laboratory_Feeding/Model/Animal.py
@@ -1,7 +1,7 @@
 import string
 
 
-class Shark:
+class Animal:
 
     def __init__(self, common_name: string, genus: string, species: string, max_length: float, max_age: int or string):
         self.velocity = 0
@@ -18,7 +18,6 @@ class Shark:
         print('Max length: ', self.max_length, 'feet')
         print('Max age: ', self.max_age, 'years')
 
-    # How do I print as a list of all the sharks with all of their attributes???
 
     def faster(self):
         self.velocity += 1
@@ -28,14 +27,14 @@ class Shark:
             self.velocity -= 1
 
 
-horn = Shark('Horn Shark', 'Heterodontus', 'francisci', 4.0, 25)
-swell = Shark('Swell Shark', 'Cephaloscyllium', 'ventriosum', 3.5, 10)
-white = Shark('Great White Shark', 'Carcharadon', 'carcharias', 20.0, 70)
-shortfin_mako = Shark('Shortfin Mako', 'Isurus', 'oxyrinchus', 12.0, 30)
-longfin_mako = Shark('Longfin Mako', 'Isurus', 'paucus', 14.0, 'Unknown')
-lemon = Shark('Lemon Shark', 'Negaprion', 'bevriostris', 11.0, 27)
-bull = Shark('Bull Shark', 'Carcharhinus', 'leucas', 11.5, 14)
-great_hammer = Shark('Great Hammerhead Shark', 'Sphyrna', 'mokarran', 20.0, 35)
-nurse = Shark('Nurse Shark', 'Ginglymostoma', 'cirratum', 14.0, 35)
-porbeagle = Shark('Porbeagle Shark', 'Lamna', 'nasus', 6.6, 65)
-black_tip = Shark('Blacktip Reef Shark', 'Carcharhinus', 'melanopterus', 5.2, 13)
+horn = Animal('Horn Shark', 'Heterodontus', 'francisci', 4.0, 25)
+swell = Animal('Swell Shark', 'Cephaloscyllium', 'ventriosum', 3.5, 10)
+white = Animal('Great White Shark', 'Carcharadon', 'carcharias', 20.0, 70)
+shortfin_mako = Animal('Shortfin Mako', 'Isurus', 'oxyrinchus', 12.0, 30)
+longfin_mako = Animal('Longfin Mako', 'Isurus', 'paucus', 14.0, 'Unknown')
+lemon = Animal('Lemon Shark', 'Negaprion', 'bevriostris', 11.0, 27)
+bull = Animal('Bull Shark', 'Carcharhinus', 'leucas', 11.5, 14)
+great_hammer = Animal('Great Hammerhead Shark', 'Sphyrna', 'mokarran', 20.0, 35)
+nurse = Animal('Nurse Shark', 'Ginglymostoma', 'cirratum', 14.0, 35)
+porbeagle = Animal('Porbeagle Shark', 'Lamna', 'nasus', 6.6, 65)
+black_tip = Animal('Blacktip Reef Shark', 'Carcharhinus', 'melanopterus', 5.2, 13)
